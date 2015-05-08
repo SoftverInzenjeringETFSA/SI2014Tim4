5d3be25ea83267678e8bf187fd77da11038803c7
EldarGranulo <apokalipso_t@hotmail.com>
2015-05-08 10:45:15 +0200

    Testni commit

--- Projekat/tim4app/src/main/java/ba/etf/unsa/si/tim4/tim4app/design/PocetniEkran.java
+++ Projekat/tim4app/src/main/java/ba/etf/unsa/si/tim4/tim4app/design/PocetniEkran.java
@@ -11,7 +11,7 @@ public class PocetniEkran {
 	private JFrame frmPoetniEkran;
 
 	/**
-	 * Launch the application.
+	 * Launch the application from here.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
