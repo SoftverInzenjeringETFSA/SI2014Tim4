56ddd781949d6a1062825cf25bd04bc7b8ba1d43
Elma Gazetic <egazetic1@gmail.com>
2015-05-25 00:46:34 +0200

    Komitent fix

--- Projekat/tim4app/src/main/java/ba/etf/unsa/si/tim4/tim4app/daldao/KomitentDataSource.java
+++ Projekat/tim4app/src/main/java/ba/etf/unsa/si/tim4/tim4app/daldao/KomitentDataSource.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.logging.Level;
+
 import ba.etf.unsa.si.tim4.tim4app.classes.FizickiKomitent;
 import ba.etf.unsa.si.tim4.tim4app.classes.Komitent;
 import ba.etf.unsa.si.tim4.tim4app.classes.PravniKomitent;
@@ -131,6 +132,8 @@ public class KomitentDataSource {
 		try {
 			ps.setInt(1, id);
 			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
 					String tipKomitenta = rs.getString(1);
 					String adresa = rs.getString(2);
 					String brojTelefona = rs.getString(3);
@@ -151,8 +154,9 @@ public class KomitentDataSource {
 						String brojLicneKarte = rs.getString(8);
 						toRet = new FizickiKomitent(ime, prezime, jmb, brojLicneKarte, id, tipKomitenta, adresa, brojTelefona, eMail);
 					}
+			}
 					dbUtils.closeCurrentConnection();
-					return toRet;
+					return null;
 		} catch (SQLException e) {
 			dbUtils.printExceptionMessage(e.getMessage(), "getKomitentById()");
 			dbUtils.logException(Level.SEVERE, e.getMessage(), e);
@@ -169,6 +173,8 @@ public class KomitentDataSource {
 		try {
 			ps.setString(1, jmb);
 			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
 					String tipKomitenta = rs.getString(1);
 					String adresa = rs.getString(2);
 					String brojTelefona = rs.getString(3);
@@ -191,7 +197,8 @@ public class KomitentDataSource {
 						dbUtils.closeCurrentConnection();
 						return new FizickiKomitent(ime, prezime, jmbg, brojLicneKarte, id, tipKomitenta, adresa, brojTelefona, eMail);
 					}
-
+			}
+			return null;
 		} catch (SQLException e) {
 			dbUtils.printExceptionMessage(e.getMessage(), "getKomitentByJMB()");
 			dbUtils.logException(Level.SEVERE, e.getMessage(), e);
@@ -202,12 +209,14 @@ public class KomitentDataSource {
 	
 	public Komitent getKomitentByLicnaKarta(String licnaKarta)
 	{
-		String query = "SELECT tip_komitenta, adresa, broj_telefona, email, ime, prezime, jmb, broj_licne_karte, nazivFirme, pdvBroj, id FROM komitenti WHERE brojLicneKarte=?";
+		String query = "SELECT tip_komitenta, adresa, broj_telefona, email, ime, prezime, jmb, broj_licne_karte, nazivFirme, pdvBroj, id FROM komitenti WHERE broj_licne_karte=?";
 		PreparedStatement ps = dbUtils.getPreparedStatement(query);
 		if(ps == null) return null;
 		try {
 			ps.setString(1, licnaKarta);
 			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
 					String tipKomitenta = rs.getString(1);
 					String adresa = rs.getString(2);
 					String brojTelefona = rs.getString(3);
@@ -228,6 +237,8 @@ public class KomitentDataSource {
 						String brojLicneKarte = rs.getString(8);
 						return new FizickiKomitent(ime, prezime, jmbg, brojLicneKarte, id, tipKomitenta, adresa, brojTelefona, eMail);
 					}
+			}
+			return null;
 
 		} catch (SQLException e) {
 			dbUtils.printExceptionMessage(e.getMessage(), "getKomitentByJMB()");
@@ -245,6 +256,8 @@ public class KomitentDataSource {
 		try {
 			ps.setString(1, pdvBrojFirme);
 			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
 					String tipKomitenta = rs.getString(1);
 					String adresa = rs.getString(2);
 					String brojTelefona = rs.getString(3);
@@ -265,6 +278,8 @@ public class KomitentDataSource {
 						String brojLicneKarte = rs.getString(8);
 						return new FizickiKomitent(ime, prezime, jmbg, brojLicneKarte, id, tipKomitenta, adresa, brojTelefona, eMail);
 					}
+			}
+			return null;
 
 		} catch (SQLException e) {
 			dbUtils.printExceptionMessage(e.getMessage(), "getKomitentByJMB()");
@@ -314,4 +329,49 @@ public class KomitentDataSource {
 			return null;
 		}
 	}
+	
+	public int getIdByjmb(String jmb)
+	{
+		String query = "SELECT id FROM komitenti WHERE jmb=?";
+		PreparedStatement ps = dbUtils.getPreparedStatement(query);
+		if(ps == null) return -1;
+		try {
+			ps.setString(1, jmb);
+			ResultSet rs = ps.executeQuery();
+				while(rs.next()){
+					int id = rs.getInt(1);
+					return id;
+				}
+			dbUtils.closeCurrentConnection();
+			return -1;
+		} catch (SQLException e) {
+			dbUtils.printExceptionMessage(e.getMessage(), "getAll()");
+			dbUtils.logException(Level.SEVERE, e.getMessage(), e);
+			dbUtils.closeCurrentConnection();
+			return -1;
+		}
+	}	
+	
+	public int getIdByNazivFirme(String naziv)
+	{
+		String query = "SELECT id FROM komitenti WHERE nazivFime=?";
+		PreparedStatement ps = dbUtils.getPreparedStatement(query);
+		if(ps == null) return -1;
+		try {
+			ps.setString(1, naziv);
+			ResultSet rs = ps.executeQuery();
+				while(rs.next()){
+					int id = rs.getInt(1);
+					return id;
+				}
+			dbUtils.closeCurrentConnection();
+			return -2;
+		} catch (SQLException e) {
+			dbUtils.printExceptionMessage(e.getMessage(), "getAll()");
+			dbUtils.logException(Level.SEVERE, e.getMessage(), e);
+			dbUtils.closeCurrentConnection();
+			return -3;
+		}
+	}
+	
 }
--- Projekat/tim4app/src/test/java/ba/etf/unsa/si/tim4/tim4app/KomitentClassTest.java
+++ Projekat/tim4app/src/test/java/ba/etf/unsa/si/tim4/tim4app/KomitentClassTest.java
@@ -12,136 +12,102 @@ public class KomitentClassTest extends TestCase {
 	
 	public void testKomitentDalDao_KomitentByJMB()
 	{
-		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
+		FizickiKomitent k;
+		KomitentDataSource kds= new KomitentDataSource();
 		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
-		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
+		
 		kds.insert(k);
 		
-		assertEquals(k,kds.getKomitentByJMB("0308964384007"));
-		//kds.delete(666);
+		int id = kds.getIdByjmb(k.getJMB());
+
+
+		FizickiKomitent k1 = (FizickiKomitent) kds.getKomitentByJMB(k.getJMB());
+	    assertEquals(k.getJMB(),k1.getJMB());
+		kds.delete(id);
 	}
 	
 	public void testKomitentDalDao_KomitentById()
 	{
-		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
+		FizickiKomitent k;
+		KomitentDataSource kds= new KomitentDataSource();
 		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
-		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
-		kds.insert(k);
 		
-		assertEquals(k,kds.getKomitentById(666));
-		kds.delete(666);
+		kds.insert(k);
+		int id = kds.getIdByjmb(k.getJMB());
+		assertTrue(id > 0);
+		kds.delete(id);
 	}
 	
 	public void testKomitentDalDao_KomitentByLicnaKarta()
 	{
-		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
+		FizickiKomitent k;
+		KomitentDataSource kds = new KomitentDataSource();
 		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
-		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
+	
 		kds.insert(k);
+		FizickiKomitent k1 = (FizickiKomitent) kds.getKomitentByLicnaKarta(k.getBrojLicneKarte());
 		
-		assertEquals(k,kds.getKomitentByLicnaKarta("12xxx6789"));
-		kds.delete(666);
+		assertEquals(k1.getBrojLicneKarte(),k.getBrojLicneKarte());
+		int id = kds.getIdByjmb(k.getJMB());
+		kds.delete(id);
 	}
 	
 	public void testKomitentDalDao_GetAll()
 	{
 		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
+		KomitentDataSource kds = new KomitentDataSource();
 		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
 		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
+		
 		LinkedList<Komitent> komitenti = kds.getAll();
-		assertNotNull(komitenti.size());
+		
 		kds.insert(k);
 		kds.insert(k1);
+		
 		LinkedList<Komitent> komitenti2 = kds.getAll();
 		assertEquals(komitenti.size(),komitenti2.size() -2 );
-		assertTrue(kds.getAll().contains(k));
-		kds.delete(666);
-		kds.delete(77);
+		
+		int id = kds.getIdByjmb(k.getJMB());
+		kds.delete(id);
+		id = kds.getIdByjmb(k1.getJMB());
+		kds.delete(id);
+
 	}
 	
 	public void testKomitentDalDao_KomitentByPDVBroj()
 	{
-		FizickiKomitent k,k1;
 		PravniKomitent pk;
-		KomitentDataSource kds;
+		KomitentDataSource kds = new KomitentDataSource();
 		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
 				"Firma doo","123123123");
-		kds = new KomitentDataSource();
-		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
-		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
+		
 		kds.insert(pk);
 		
-		assertEquals(pk,kds.getKomitentByPDVBroj("123123123"));
-		kds.delete(333);
-	}
-	
-	public void testKomitentDalDao_Update()
-	{
-		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
-		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
-		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
-		kds.insert(pk);
-		pk.setAdresa("Malta");
-		kds.update(pk);
-		assertEquals("Malta",kds.getKomitentByPDVBroj("123123123").getAdresa());
-		kds.delete(333);
+		PravniKomitent pravni = (PravniKomitent) kds.getKomitentByPDVBroj(pk.getPDVbroj());
+		assertEquals(pk.getNazivFirme(),pravni.getNazivFirme());
+		int id = kds.getIdByNazivFirme(pk.getNazivFirme());
+		kds.delete(id);
 	}
 	
 	public void testKomitentDalDao_Insert()
 	{
 		FizickiKomitent k,k1;
-		PravniKomitent pk;
-		KomitentDataSource kds;
-		 pk = new PravniKomitent(333,"Pravno lice", "Grbavica","061666666","email@gmail.com",
-				"Firma doo","123123123");
-		kds = new KomitentDataSource();
+		KomitentDataSource kds = new KomitentDataSource();
 		k = new FizickiKomitent("Elma","Gazetic","0308964384007","12xxx6789","Fizicko lice","Sarajevo","062444444","egazetic1@etf.unsa.ba");
 		k1 = new FizickiKomitent("Demir","Gazetic","1304005111059","12xxx6789","Fizicko lice","Sarajevo","062444446","dgazetic1@etf.unsa.ba");
-		k.setId(666);
-		k1.setId(77);
+		
+		LinkedList<Komitent> beforekomitenti = kds.getAll();
 		kds.insert(k);
 		kds.insert(k1);
 		LinkedList<Komitent> komitenti = kds.getAll();
-		assertTrue(komitenti.contains(k));
-		assertTrue(komitenti.contains(k1));
-		kds.delete(666);
-		kds.delete(77);
+		
+		assertEquals(komitenti.size(),beforekomitenti.size() + 2);
+		
+		int id = kds.getIdByjmb(k.getJMB());
+		kds.delete(id);
+		id = kds.getIdByjmb(k1.getJMB());
+		kds.delete(id);
 	}
+	
+	
 }
