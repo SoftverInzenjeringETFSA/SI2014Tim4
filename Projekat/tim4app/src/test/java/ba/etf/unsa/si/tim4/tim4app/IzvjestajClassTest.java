a7a1145e2a02f5e700436e89f6ab380a2adb4b90
NadinHasanic <nhasanic1@etf.unsa.ba>
2015-05-24 22:32:11 +0200

    Izvjestaj ispravljeni testovi

--- Projekat/tim4app/src/test/java/ba/etf/unsa/si/tim4/tim4app/IzvjestajClassTest.java
+++ Projekat/tim4app/src/test/java/ba/etf/unsa/si/tim4/tim4app/IzvjestajClassTest.java
@@ -11,28 +11,24 @@ import java.util.Date;
 
 public class IzvjestajClassTest extends TestCase {
 
-		Izvjestaj iz;
-		Date datum;
-		IzvjestajiDataSource ids;
-		protected void SetUp()
-		{
-			 ids = new IzvjestajiDataSource();
-			SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
-			datum = null;
-			try 
-			{
-				datum = dateformat.parse("24/05/2015");
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			
-			iz = new Izvjestaj("45555", datum, "parametar","Izvjestaj trenutnog stanja za komitenta");
-		}
+		
+
 		
 		public void testIzvjestaj_Konstruktor()
 		{
-			
-			Izvjestaj iz = new Izvjestaj("45555", datum, "parametar","Izvjestaj trenutnog stanja za komitenta");
+			Date datum;
+			IzvjestajiDataSource ids;
+			ids = new IzvjestajiDataSource();
+				SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+				datum = null;
+				try 
+				{
+					datum = dateformat.parse("24/05/2015");
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				
+				Izvjestaj iz = new Izvjestaj("45555", datum, "parametar","Izvjestaj trenutnog stanja za komitenta");
 			
 			assertEquals("45555",iz.getBrojIzvjestaja());
 			assertEquals(datum,iz.getDatumIzvjestaja());
@@ -45,6 +41,20 @@ public class IzvjestajClassTest extends TestCase {
 		
 		public void testIzvjestajiDalDao_Insert()
 		{
+			Izvjestaj iz;
+			Date datum;
+			IzvjestajiDataSource ids;
+			ids = new IzvjestajiDataSource();
+				SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+				datum = null;
+				try 
+				{
+					datum = dateformat.parse("24/05/2015");
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				
+				iz = new Izvjestaj("45555", datum, "parametar","Izvjestaj trenutnog stanja za komitenta");
 			int br =  ids.getMaxId();
 			iz.setId(br + 1);
 			ids.insert(iz);
@@ -58,13 +68,27 @@ public class IzvjestajClassTest extends TestCase {
 		
 		public void testIzvjestajiDalDao_GetMaxId()
 		{
+			Izvjestaj iz;
+			Date datum;
+			IzvjestajiDataSource ids;
+			ids = new IzvjestajiDataSource();
+				SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+				datum = null;
+				try 
+				{
+					datum = dateformat.parse("24/05/2015");
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				
+				iz = new Izvjestaj("45555", datum, "parametar","Izvjestaj trenutnog stanja za komitenta");
 			int br =  ids.getMaxId();
 			iz.setId(br + 1);
 			ids.insert(iz);
 			
 			int br2= ids.getMaxId();
 			
-			assertEquals(br,br2-1);
+			assertEquals(br,br2-2);
 			ids.delete(br2);
 		}
 
